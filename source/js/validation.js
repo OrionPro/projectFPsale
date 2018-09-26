af0f2b6aa430afb1a6a3107f4e6054e07d27ab99
OrionPro <orion79@rambler.ru>
2018-09-26 18:51:36 +0300

    18 05 26.09.19 убрал обработку submit

--- source/js/validation.js
+++ source/js/validation.js
@@ -125,135 +125,135 @@ $(document).ready(function () {
 			}
 		});
 	});
-	$("form:not('#form-file')").submit(function () {
-		if ($(this).valid()) {
-			let self = $(this);
-			let data = self.serialize();
-			$.ajax({
-				type: "POST",
-				url: "./mail.php",
-				data: data,
-				dataType: "json",
-				beforeSend: function () { // событие до отправки
-					self.find('input[type="submit"]').attr('disabled', 'disabled'); // например, отключим кнопку, чтобы не жали по 100 раз
-				},
-				success: function (data) {
-					if (data['form_type'] == 'modal') {
-						$('.white-popup form').hide();
-						$('.white-popup .mfp-close').hide();
-						self.trigger('reset');
-						$('.white-popup .success_mail').addClass('active');
-						setTimeout(function () {
-							$.magnificPopup.close();
-						}, 2500);
-						setTimeout(function () {
-							$('.white-popup form').show();
-							$('.white-popup .mfp-close').show();
-							$('.white-popup .input_wrap i').hide();
-							$('.white-popup .success_mail').removeClass('active');
-							self.find('input[type="submit"]').attr('disabled', false);
-						}, 3500);
-					}
-					if (data['form_type'] == 'normal') { //надо писать в обычных формах <input type="hidden" name="form_type" value="normal">
-						self.trigger('reset');
-						$.magnificPopup.open({
-							items: {
-								src: $('#popup-success')
-							},
-							type: 'inline'
-						});
-						setTimeout(function () {
-							$.magnificPopup.close();
-							$('form .input_wrap i').hide();
-							self.find('input[type="submit"]').attr('disabled', false);
-						}, 3000);
-					}
-				}
-			});
-		}
-		return false;
-	});
-
-//  Отправка форм с файлом вносим input[type=file]
-	let files;
-	$('input[type=file][name=field]').change(function () {
-		files = this.files;
-	});
-//  Отправка форм с файлом submit
-	$("#form-file").on('submit', function (e) { // перехватываем все при событии отправки
-		if ($(this).valid()) {
-			let $data = new FormData(),
-				form = $(this),
-				$inputs = $("#form-file").find('input[type=hidden]'),
-				$phone = $("#form-file").find('input[name=phone]'),
-				$email = $("#form-file").find('input[name=email]'),
-				$name = $("#form-file").find('input[name=name]'),
-				$textarea = $("#form-file").find('textarea');
-
-			$.each(files, function (key, value) {
-
-				$data.append(key, value);
-			});
-
-			$.each($inputs, function (key, value) {
-				$data.append($(this).attr('name'), $(this).val());
-			});
-
-			//добавление основных тестовых полей вместо serialize
-			$data.append($textarea.attr('name'), $textarea.val());
-			$data.append($phone.attr('name'), $phone.val());
-			$data.append($email.attr('name'), $email.val());
-			$data.append($name.attr('name'), $name.val());
-
-			$.ajax({
-				url: './mail.php',
-				type: 'POST',
-				contentType: false,
-				processData: false,
-				dataType: 'json',
-				data: $data,
-				beforeSend: function (loading) {
-					$('input[type=file]').tooltipster('content', "Файл загружается");
-					$('input[type=file]').tooltipster('show');
-				},
-				success: function (data) {
-					if (data['form_type'] == 'normal') { //надо писать в обычных формах <input type="hidden" name="form_type" value="normal">
-						$('input[type=file]').tooltipster('content', "Файл загружен!");
-						$('input[type=file]').tooltipster('show');
-						form.tooltipster('content', "Письмо отправлено!");
-						form.tooltipster('show');
-						setTimeout(function () {
-							form.trigger('reset');
-							form.find('i').hide();
-							files = undefined;
-							$('input[type=file]').tooltipster('hide');
-						}, 2000);
-						setTimeout(function () {
-							form.tooltipster('hide');
-						}, 3500);
-						// Вариант с показом модального окна с success
-						// $('input[type=file]').tooltipster('content', "Файл загружен!");
-						// $('input[type=file]').tooltipster('show');
-						// self.trigger('reset');
-						// $.magnificPopup.open({
-						// 	items: {
-						// 		src: $('#popup-success')
-						// 	},
-						// 	type: 'inline'
-						// });
-						// //$("body").css({ "overflow": "hidden", "padding-right": "17px" });
-						// setTimeout(function () {
-						// 	$.magnificPopup.close();
-						// 	$('form').find('i').hide();
-						// 	files = undefined;
-						// 	self.find('input[type="submit"]').attr('disabled', false);
-						// 	//$("body").css({ "overflow": "inherit", "padding-right": "0" });
-						// }, 3000);
-					}
-
-				}
-			});
-		}
-		return false;
-	});
+// 	$("form:not('#form-file')").submit(function () {
+// 		if ($(this).valid()) {
+// 			let self = $(this);
+// 			let data = self.serialize();
+// 			$.ajax({
+// 				type: "POST",
+// 				url: "./mail.php",
+// 				data: data,
+// 				dataType: "json",
+// 				beforeSend: function () { // событие до отправки
+// 					self.find('input[type="submit"]').attr('disabled', 'disabled'); // например, отключим кнопку, чтобы не жали по 100 раз
+// 				},
+// 				success: function (data) {
+// 					if (data['form_type'] == 'modal') {
+// 						$('.white-popup form').hide();
+// 						$('.white-popup .mfp-close').hide();
+// 						self.trigger('reset');
+// 						$('.white-popup .success_mail').addClass('active');
+// 						setTimeout(function () {
+// 							$.magnificPopup.close();
+// 						}, 2500);
+// 						setTimeout(function () {
+// 							$('.white-popup form').show();
+// 							$('.white-popup .mfp-close').show();
+// 							$('.white-popup .input_wrap i').hide();
+// 							$('.white-popup .success_mail').removeClass('active');
+// 							self.find('input[type="submit"]').attr('disabled', false);
+// 						}, 3500);
+// 					}
+// 					if (data['form_type'] == 'normal') { //надо писать в обычных формах <input type="hidden" name="form_type" value="normal">
+// 						self.trigger('reset');
+// 						$.magnificPopup.open({
+// 							items: {
+// 								src: $('#popup-success')
+// 							},
+// 							type: 'inline'
+// 						});
+// 						setTimeout(function () {
+// 							$.magnificPopup.close();
+// 							$('form .input_wrap i').hide();
+// 							self.find('input[type="submit"]').attr('disabled', false);
+// 						}, 3000);
+// 					}
+// 				}
+// 			});
+// 		}
+// 		return false;
+// 	});
+//
+// //  Отправка форм с файлом вносим input[type=file]
+// 	let files;
+// 	$('input[type=file][name=field]').change(function () {
+// 		files = this.files;
+// 	});
+// //  Отправка форм с файлом submit
+// 	$("#form-file").on('submit', function (e) { // перехватываем все при событии отправки
+// 		if ($(this).valid()) {
+// 			let $data = new FormData(),
+// 				form = $(this),
+// 				$inputs = $("#form-file").find('input[type=hidden]'),
+// 				$phone = $("#form-file").find('input[name=phone]'),
+// 				$email = $("#form-file").find('input[name=email]'),
+// 				$name = $("#form-file").find('input[name=name]'),
+// 				$textarea = $("#form-file").find('textarea');
+//
+// 			$.each(files, function (key, value) {
+//
+// 				$data.append(key, value);
+// 			});
+//
+// 			$.each($inputs, function (key, value) {
+// 				$data.append($(this).attr('name'), $(this).val());
+// 			});
+//
+// 			//добавление основных тестовых полей вместо serialize
+// 			$data.append($textarea.attr('name'), $textarea.val());
+// 			$data.append($phone.attr('name'), $phone.val());
+// 			$data.append($email.attr('name'), $email.val());
+// 			$data.append($name.attr('name'), $name.val());
+//
+// 			$.ajax({
+// 				url: './mail.php',
+// 				type: 'POST',
+// 				contentType: false,
+// 				processData: false,
+// 				dataType: 'json',
+// 				data: $data,
+// 				beforeSend: function (loading) {
+// 					$('input[type=file]').tooltipster('content', "Файл загружается");
+// 					$('input[type=file]').tooltipster('show');
+// 				},
+// 				success: function (data) {
+// 					if (data['form_type'] == 'normal') { //надо писать в обычных формах <input type="hidden" name="form_type" value="normal">
+// 						$('input[type=file]').tooltipster('content', "Файл загружен!");
+// 						$('input[type=file]').tooltipster('show');
+// 						form.tooltipster('content', "Письмо отправлено!");
+// 						form.tooltipster('show');
+// 						setTimeout(function () {
+// 							form.trigger('reset');
+// 							form.find('i').hide();
+// 							files = undefined;
+// 							$('input[type=file]').tooltipster('hide');
+// 						}, 2000);
+// 						setTimeout(function () {
+// 							form.tooltipster('hide');
+// 						}, 3500);
+// 						// Вариант с показом модального окна с success
+// 						// $('input[type=file]').tooltipster('content', "Файл загружен!");
+// 						// $('input[type=file]').tooltipster('show');
+// 						// self.trigger('reset');
+// 						// $.magnificPopup.open({
+// 						// 	items: {
+// 						// 		src: $('#popup-success')
+// 						// 	},
+// 						// 	type: 'inline'
+// 						// });
+// 						// //$("body").css({ "overflow": "hidden", "padding-right": "17px" });
+// 						// setTimeout(function () {
+// 						// 	$.magnificPopup.close();
+// 						// 	$('form').find('i').hide();
+// 						// 	files = undefined;
+// 						// 	self.find('input[type="submit"]').attr('disabled', false);
+// 						// 	//$("body").css({ "overflow": "inherit", "padding-right": "0" });
+// 						// }, 3000);
+// 					}
+//
+// 				}
+// 			});
+// 		}
+// 		return false;
+// 	});
 });
